bcfc6afcbd30daedb8bcfc2f76a420b2b3840f00
mateuslpds <mateusyasmim2014@gmail.com>
2025-03-11 21:33:29 -0300

    Corrigindo problema com css

--- GameSync_project/GameSync_project/settings.py
+++ GameSync_project/GameSync_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+gn-=^j&m-k84g8tf8gdq5y8-6j@m^d-dp!&dn#ohfl0l2@r-y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'gamesync.onrender.com',
