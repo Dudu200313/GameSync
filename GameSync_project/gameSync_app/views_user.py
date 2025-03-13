99aa98ada17a534ebd2da70fb48b8bca0cf25158
mateuslpds <mateusyasmim2014@gmail.com>
2025-03-13 02:56:31 -0300

    Adicionando redirecionamento para home após login

--- GameSync_project/gameSync_app/views_user.py
+++ GameSync_project/gameSync_app/views_user.py
@@ -39,7 +39,7 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponse ('usuario logado') 
+            return redirect('index')
         else:
             return render(request, 'login.html', {'error': 'Usuário ou senha inválidos.'})
 
